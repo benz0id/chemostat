79812934c75bf13bd6aac66a9ea8f00c1d44eb16
benz0id <bentudorprice@gmail.com>
2023-05-29 21:34:17 -0400

    initial commit. mainly structure with some device funcionality

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import time
 import src.log_config as log_config
 from src.controller_builder import Builder
+import src.gpio_adapter
 from src.global_constants import *
 
-log_config.config_loggers()
+def main():
+    log_config.config_loggers()
 
-builder = Builder()
+    builder = Builder()
 
-while True:
-    builder.mc.cycle_check()
-    builder.sm.update_readings()
-    builder.tc.regulate_temp()
-    builder.bc.regulate_airflow()
-    time.sleep(FAST_PAUSE_TICK)
+    while True:
+        builder.mc.cycle_check()
+        builder.sm.update_readings()
+        builder.tc.regulate_temp()
+        builder.bc.regulate_airflow()
+        time.sleep(FAST_PAUSE_TICK)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception:
+        src.gpio_adapter.get_GPIO().cleanup()
+
 
 
 
